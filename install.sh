af26116bf3d9294d04ad3c3f87a5cf0e8b0da5ba
alexesba <alexesba@gmail.com>
2015-04-17 12:08:19 -0500

    update installer

--- install.sh
+++ install.sh
@@ -2,10 +2,29 @@
 
 if [ -a $HOME/.vimrc ]
 then
-  echo "Backup .vimrc file to .vimrc_old"
-  mv $HOME/.vimrc $HOME/.vimrc_old
+  echo "Do you wish to backup your .vimrc file?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes )
+            echo "Backup .vimrc file to .vimrc_old"
+            mv $HOME/.vimrc $HOME/.vimrc_old
+          break;;
+          No )
+            rm $HOME/.vimrc
+            exit;;
+      esac
+  done
 fi
 
+echo "Do you wish to install the bash aliases?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+          ln -s $HOME/.vim/.bash_aliases $HOME/.bash_aliases
+        break;;
+        No ) exit;;
+    esac
+done
 
 echo "initializing submodules"
 git submodule init
